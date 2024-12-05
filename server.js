d82d7b8f3daab000fae49fc1de3ac3b247fce299
DiegoPatterson <DiegoChristopher.Patterson@calbaptist.edu>
2024-12-05 12:08:55 -0800

    Copilot solution

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the 'lib/Screens' directory
-app.use(express.static(path.join(__dirname, 'lib')));
+// Serve static files from the root directory
+app.use(express.static(__dirname));
 
-// Serve the HTML file
+// Serve the HTML file for the root route
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'lib', 'Screens', 'HomePage.html'));
 });
